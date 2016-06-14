b87510c78a4a6b58a75164ec405f22c662bb812e
gordonseto <gordon.seto@gmail.com>
2016-06-14 16:27:54 -0600

    added 'add post' view

--- my-hood/AddPostVC.swift
+++ my-hood/AddPostVC.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class AddPostVC: UIViewController {
 
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        postImg.layer.cornerRadius = postImg.frame.size.width / 2
+        postImg.clipsToBounds = true
     }
     
+    @IBAction func postButtonPressed(sender: UIButton!) {
+        sender.setTitle("", forState: .Normal)
+    }
+    
+    @IBAction func cancelButtonPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func addPicButtonPressed(sender: AnyObject) {
+    }
     
 }
